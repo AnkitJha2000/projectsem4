3f3e2a03fc732ccb269292d8ed245c61cd8edc79
AnkitJha2000 <ankitsakshi9800@gmail.com>
2021-06-18 00:32:57 +0530

    date 17/06/2021 1. home fragment started 2. ui images start

--- app/src/main/java/com/example/projectsem4/activities/parent/fragments/ParentHomePage.kt
+++ app/src/main/java/com/example/projectsem4/activities/parent/fragments/ParentHomePage.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.projectsem4.ViewModels.FirebaseAuthViewModel
 import com.example.projectsem4.databinding.FragmentParentHomePageBinding
 
 class ParentHomePage : Fragment() {
     private lateinit var binding : FragmentParentHomePageBinding
+    private val viewModel : FirebaseAuthViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentParentHomePageBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
 }
